a3db4ef98696d2c39aace5a8dc75f8ded086c21b
tingyu <pengwei_outlook.com>
2022-08-29 17:53:21 +0800

    autostart.sh中增加输入法和壁纸的自启动

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
+while true; do
+   xsetroot -name "$(date +"%F %R")"
+   sleep 1m    # Update time every minute
+done &
 xrandr -s 1440x900 &
+feh --bg-scale ~/Downloads/1.png &
 fcitx5 -d &
